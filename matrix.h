a80dcb82910ea3699c97133dad3d24edbe351c3d
Michael Chen <cwchen@debian>
2016-12-22 06:42:09 +0800

    Update header

--- matrix.h
+++ matrix.h
@@ -1,17 +1,17 @@
-#ifndef __MATRIX_H_
-#define __MATRIX_H_
+#ifndef __MATRIX_H__
+#define __MATRIX_H__
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
   void* matrix_new(size_t, size_t);
-  double matrix_get_at(void*, size_t, size_t);
-  void matrix_set_at(void*, double, size_t, size_t);
+  double matrix_get(void*, size_t, size_t);
+  void matrix_set(void*, size_t, size_t, double);
   void matrix_free(void*);
 
 #ifdef __cplusplus
 }
 #endif
 
-#endif // __MATRIX_H_
+#endif  // __MATRIX_H__
